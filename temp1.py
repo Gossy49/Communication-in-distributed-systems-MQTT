1d709bb6f138e99cf8315c1854783897655b07f2
God'stime Okolo <godstimeokolo25@gmail.com>
2026-01-06 01:36:40 +0100

    Add temperature sensor T1 publisher

---
+++ temp1.py
@@ -0,0 +1,58 @@
+import time 
+import random 
+import json
+import paho.mqtt.client as mqtt
+from pydantic import BaseModel
+
+
+class TemperaturesData(BaseModel):
+    sensor_name: str
+    temperature: float
+
+def on_connect(client, userdata, flags, rc, properties):
+    if rc == 0:
+        print("Connected successfully to the broker.")
+    else:
+        print(f"Connection failed with code {rc}")
+
+if __name__ == '__main__':
+    sensor_name ="T1"
+
+
+    # Create an MQTT client instance
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+    # Set callback for connection
+    client.on_connect = on_connect
+
+    # Connect to the broker
+    client.connect("localhost", 1883, 60)
+
+    # Start the loop
+    client.loop_start()
+
+  
+
+    try:
+        while True:
+            # Random temperature: 0 to 30
+            temp_value = round(random.uniform(0, 30),2)
+
+            #validate with pydantic
+            data = TemperaturesData(sensor_name=sensor_name, temperature=temp_value)
+           
+            # Convert to JSON string (simple way)
+            payload = json.dumps(data.model_dump())
+
+            # Publish to "temp" topic
+            client.publish("temp", payload)
+            print("Published:", payload)
+
+            time.sleep(2)
+
+    except KeyboardInterrupt:
+        print("Temperature1 stopped.")
+
+    # Stop the loop and disconnect
+    client.loop_stop()
+    client.disconnect()

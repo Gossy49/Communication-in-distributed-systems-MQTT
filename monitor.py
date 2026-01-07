558a3f3033cf8c75b9beac8ae4a97a501dd9705b
God'stime Okolo <godstimeokolo25@gmail.com>
2026-01-07 11:48:32 +0100

    Fix monitor temperature validation and print formating

--- monitor.py
+++ monitor.py
@@ -1,4 +1,12 @@
+import json
 import paho.mqtt.client as mqtt
+from pydantic import BaseModel
+
+
+class TemperatureData(BaseModel):
+    sensor_name  : str 
+    temperature: float
+
 
 def on_connect(client, userdata, flags, rc, properties):
     print("connected with result code", rc)
@@ -8,7 +16,30 @@ def on_connect(client, userdata, flags, rc, properties):
 
 
 def on_message(client, userdata, msg ):
-    print(f"Received: {msg.topic} {msg.payload}")
+
+    # having in mind that the payload is in bytes so here it's converted to string
+
+    text =msg.payload.decode("utf-8")
+
+    if msg.topic == "temp":
+        try: 
+            # converts from json string to dictionary 
+            data_dict= json.loads(text)
+
+            #dict to validates obj
+            data = TemperatureData(
+                sensor_name=data_dict["sensor_name"],
+                temperature=data_dict["temperature"]
+            )
+
+            print(data.sensor_name, "temperature:", round(data.temperature, 2))
+
+        except Exception as e:
+            print(f"Bad temp message: {text}")
+            print(f"Error: {e}")
+    else:
+        print(f"Received on {msg.topic}: {text}")
+
 
 if __name__ == "__main__":
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -24,3 +55,4 @@ if __name__ == "__main__":
 
     except KeyboardInterrupt:
         print("Monitor stopped.")
+        client.disconnect()

cb127713aba4f71179c787859d071dc42d7c193c
God'stime Okolo <godstimeokolo25@gmail.com>
2026-01-07 13:21:24 +0100

    Monitor projects humidity messages

--- monitor.py
+++ monitor.py
@@ -7,6 +7,10 @@ class TemperatureData(BaseModel):
     sensor_name  : str 
     temperature: float
 
+class HumidityData(BaseModel):
+    sensor_name : str
+    humidity : float
+
 
 def on_connect(client, userdata, flags, rc, properties):
     print("connected with result code", rc)
@@ -31,11 +35,23 @@ def on_message(client, userdata, msg ):
                 sensor_name=data_dict["sensor_name"],
                 temperature=data_dict["temperature"]
             )
-
-            print(data.sensor_name, "temperature:", round(data.temperature, 2))
+            print(f"{data.sensor_name} temperature: {data.temperature:.2f}")
 
         except Exception as e:
             print(f"Bad temp message: {text}")
+            print(f"Error: {e}")
+    
+    elif msg.topic == "hum":
+        try:
+            data_dict = json.loads(text)
+            data = HumidityData(
+                sensor_name=data_dict ["sensor_name"],
+                humidity = data_dict ["humidity"]
+            )
+            print(f"{data.sensor_name} humidity:{data.humidity:.2f}%")
+
+        except Exception as e:
+            print(f"Bad hum message: {text}")
             print(f"Error: {e}")
     else:
         print(f"Received on {msg.topic}: {text}")
